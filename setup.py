a5e86b9c2a1da999da4f56a845b494a4557ba698
laperlej <jonathan.laperle@usherbrooke.ca>
2018-01-10 16:38:32 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+import os
+from setuptools import setup
+
+setup(
+    name = "epigeec",
+    version = "0.9",
+    author = "Jonathan Laperle",
+    author_email = "jonathan.laperle@usherbrooke.ca",
+    description = ("TODO"),
+    scripts=["bin/epigeec"],
+    license = "GPL",
+)
