94003a4424330f595e5eb99c68f7a6a3a6bbe048
laperlej <jonathan.laperle@usherbrooke.ca>
2018-01-16 01:06:54 -0500

    release 1.3.4 python3 compatible

--- epigeec/python/core/config.py
+++ epigeec/python/core/config.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import, division, print_function
 
 import os.path
 
-VERSION = "1.3.2"
+VERSION = "1.3.4"
 
 CORE_DIR = os.path.dirname(os.path.realpath(__file__))
 PY_DIR = os.path.dirname(CORE_DIR)
